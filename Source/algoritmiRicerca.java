0fcc33790b50a4120a662df55fd3b008a6d466b7
tomma <itis@tommyspada.cok>
2024-03-15 13:18:23 +0100

    aggiunti esercizi

--- Source/algoritmiRicerca.java
+++
@@ -1,9 +0,0 @@
-public class algoritmiRicerca {
-    public static int occurrences(int[] array, int key, int occurrences){
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == key)
-                occurrences++;
-        }
-        return occurrences;
-    }
-}
